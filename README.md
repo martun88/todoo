9959ecb6b7c5da66d1cc48f7f37ff306c70a2179
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-01-01 14:44:47 +0700

    show list data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world
+# Simple Todoo (VueJs + Rest API)
 
 ## Project setup
 ```
@@ -19,6 +19,3 @@ yarn build
 ```
 yarn lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header></Header>
+    <AddTodo v-on:add-todo='addTodo'></AddTodo>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
+import Header from './components/layouts/Header'
+import axios from 'axios';
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+      ]
+    };
+  },
+  methods:{
+    deleteTodo(e){
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${e}`)
+      .then(()=>this.todos = this.todos.filter(todo=>todo.id !== e))
+      .catch()
+      this.todos = this.todos.filter(todo=>todo.id !== e);
+    },
+    addTodo(newTodo){
+      const {title:title,completed:completed} = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos',{title,completed})
+      .then(res=>this.todos=[...this.todos,res.data]).catch()
+    }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res=>this.todos = res.data)
+    .catch();
+  },
+  showTodo(){
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res=>this.todos = res.data)
+    .catch();
+  },
+  
+  completedTodo(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?completed=true')
+    .then(res=>this.todos = res.data)
+  },
+  uncompletedTodo(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?completed=false')
+    .then(res=>this.todos = res.data)
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn-hover{
+  background: #666;
 }
 </style>
